ea65a1ba4d70a0372ee99c2c3f367ae1e8e57120
vietanh2508 <vva250806@gmail.com>
2025-09-25 22:11:45 +0700

    HW1: Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main(String[] args){
+        System.out.print("Vũ Việt Anh" + "\t" + "24020033" + "\t" + "K69I-IT6");
+        System.out.println("\t" + "vietanh2508" + "\t" + "vva250806@gmail.com");
+
+        for(int i = 9;i > 0;i --){
+            System.out.println(i + " bottles of beer on the wall, " + i +" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
